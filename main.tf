e4f148d685711d2462de4bba3231ffe3bb28167e
Dmytro Domin <mr.diomin@gmail.com>
2022-04-27 23:58:44 +0300

    azure example implemented

--- main.tf
+++ main.tf
@@ -1,26 +1,20 @@
+# Configure the Azure provider
 terraform {
   required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = ">= 2.13.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
     }
   }
+
+  required_version = ">= 1.1.0"
 }
 
-provider "docker" {
-  host    = "npipe:////.//pipe//docker_engine"
+provider "azurerm" {
+  features {}
 }
 
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
-}
-
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "tutorial"
-  ports {
-    internal = 80
-    external = 8000
-  }
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
 }
